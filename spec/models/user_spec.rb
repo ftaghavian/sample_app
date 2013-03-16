849a871a4eee4769a7a5891e8f3041aebd292e12
ftaghavian <fatemeh.taghavian@gmail.com>
2013-03-16 15:43:34 +0330

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -98,7 +98,7 @@ describe User do
 
   describe "return value of authenticate method" do
     before { @user.save }
-    let(:found_user) { User.find_by_email(@user.email) }
+    let(:found_user) { User.find_by_email(@user.email) } end
 
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
